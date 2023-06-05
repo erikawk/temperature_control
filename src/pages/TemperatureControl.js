2d9ef732031b224555f37c3db6ec925e5e377182
erikawk <105462871+erikawk@users.noreply.github.com>
2023-06-05 18:24:49 -0300

    .

--- src/pages/TemperatureControl.js
+++ src/pages/TemperatureControl.js
@@ -1,7 +1,37 @@
+import axios from "axios";
 import Data from "../components/Data";
 import "./TemperatureControl.css";
+import { useEffect, useState } from "react";
 
 const TemperatureControl = () => {
+  const [umid, setUmid] = useState(0)
+  const [temp, setTemp] = useState(0)
+  const obterDadosVariavel = async (bucketToken, nomeVariavel) => {
+    return axios.get('https://api.tago.io/data', {
+        headers: {
+            'Content-Type': 'application/json',
+            'Device-Token': bucketToken
+        },
+        params: {
+            variable: nomeVariavel
+        }
+    })
+        .then(response => {
+            if (nomeVariavel==="umidade") setUmid(response.data.result[0].value)
+            else setTemp(response.data.result[0].value)
+        })
+        .catch(error => {
+            console.error(error)
+            throw error
+        })
+}
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      obterDadosVariavel("5b513aed-17a5-47c1-afee-1be13933add9", "umidade")
+      obterDadosVariavel("5b513aed-17a5-47c1-afee-1be13933add9", "temperatura")
+    }, 1500)
+    return () => clearInterval(interval)
+  }, [])
   return (
     <>
       <div className="container__principal">
@@ -10,10 +40,10 @@ const TemperatureControl = () => {
         </h4>
         <div className="container__temperature__principal">
           <div className="container__temperature">
-            <Data temperature={"20°C"} />
+            <Data temperature={temp+"°C"} />
           </div>
           <div className="container__temperature">
-            <Data moisture={"40%"} />
+            <Data moisture={umid+"%"} />
           </div>
         </div>
       </div>
